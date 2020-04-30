e637d601f08684d7e263670ce4e6b31ff150acba
Misha <misha>
2020-04-30 11:43:31 +0300

    development config with dataLoader

---
+++ src/main/java/tacos/DevelopmentConfig.java
@@ -0,0 +1,47 @@
+package tacos;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import tacos.model.Ingredient;
+import tacos.model.User;
+import tacos.repo.IngredientRepository;
+import tacos.repo.UserRepository;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+@Profile("!prod")
+public class DevelopmentConfig {
+
+    @Bean
+    public CommandLineRunner dataLoader(IngredientRepository ingredientRepository, UserRepository userRepository, PasswordEncoder encoder) {
+        return args -> {
+            List<Ingredient> ingredients = Arrays.asList(
+                    new Ingredient("FLTO", "Flour Tortilla", Ingredient.Type.WRAP),
+                    new Ingredient("COTO", "Corn Tortilla", Ingredient.Type.WRAP),
+                    new Ingredient("GRBF", "Ground Beef", Ingredient.Type.PROTEIN),
+                    new Ingredient("CARN", "Carnitas", Ingredient.Type.PROTEIN),
+                    new Ingredient("TMTO", "Diced Tomatoes", Ingredient.Type.VEGGIES),
+                    new Ingredient("LETC", "Lettuce", Ingredient.Type.VEGGIES),
+                    new Ingredient("CHED", "Cheddar", Ingredient.Type.CHEESE),
+                    new Ingredient("JACK", "Monterrey Jack", Ingredient.Type.CHEESE),
+                    new Ingredient("SLSA", "Salsa", Ingredient.Type.SAUCE),
+                    new Ingredient("SRCR", "Sour Cream", Ingredient.Type.SAUCE)
+            );
+            User testUser = new User("test",
+                    encoder.encode("test"),
+                    "fullNameTest",
+                    "streetTest",
+                    "cityTest",
+                    "stateTest",
+                    "zipTest",
+                    "+79991234567");
+            ingredients.forEach(ingredientRepository::save);
+            userRepository.save(testUser);
+        };
+    }
+}
