d5cf5bcd62559187c65aef9f5c5161d467ac8747
Misha <misha>
2020-05-03 14:53:49 +0300

    added custom endpoint RecentTacoController (api/tacos/recent)

--- src/main/java/tacos/repo/TacoRepository.java
+++ src/main/java/tacos/repo/TacoRepository.java
@@ -1,10 +1,10 @@
 package tacos.repo;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 import tacos.model.Taco;
 
 import java.util.Optional;
 
-public interface TacoRepository extends CrudRepository<Taco, String> {
+public interface TacoRepository extends PagingAndSortingRepository<Taco, String> {
     Optional<Taco> findById(Long id);
 }
---
+++ src/main/java/tacos/web/RecentTacoController.java
@@ -0,0 +1,37 @@
+package tacos.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.Link;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import tacos.model.Taco;
+import tacos.model.resources.TacoRepresentationModel;
+import tacos.model.resources.TacoRepresentationModelAssembler;
+import tacos.repo.TacoRepository;
+
+@RepositoryRestController
+public class RecentTacoController {
+    private TacoRepository tacoRepository;
+
+    @Autowired
+    public RecentTacoController(TacoRepository tacoRepository) {
+        this.tacoRepository = tacoRepository;
+    }
+
+    @GetMapping(path = "/tacos/recent", produces = "application/hal+json")
+    @ResponseBody
+    public CollectionModel<TacoRepresentationModel> recentTacos() {
+        PageRequest pageRequest = PageRequest.of(0, 12, Sort.by("createdAt").descending());
+        Iterable<Taco> all = tacoRepository.findAll(pageRequest);
+        CollectionModel<TacoRepresentationModel> modelTacos = new TacoRepresentationModelAssembler(DesignTacoController.class).toCollectionModel(all);
+        Link link = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(DesignTacoController.class).recentTacos())
+                .withRel("recents");
+        modelTacos.add(link);
+        return modelTacos;
+    }
+}
